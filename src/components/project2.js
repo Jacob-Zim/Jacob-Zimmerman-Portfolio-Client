8f3e23f1daf295e2c48675466f1cfc8ba7b78f3a
JacobAZimmerman <ijacobzim@gmail.com>
2018-07-13 14:37:54 -0700

    aria

--- src/components/project2.js
+++ src/components/project2.js
@@ -40,16 +40,16 @@ class Project2 extends Component {
                         <a className="github-client" target="_blank" rel="noopener noreferrer" href="https://github.com/Jacob-Zim/skatespots-client">Github Client</a>
                         <a className="github-server" target="_blank" rel="noopener noreferrer" href="https://github.com/Jacob-Zim/skatespots-server">Github Server</a>
                     </div>
-                    <img className="wanderlust-photo" src={pic1lg}/>
-                    <img className="wanderlust-photo-md" src={pic1md}/>
-                    <img className="wanderlust-photo-sm" src={pic1sm}/>
+                    <img className="wanderlust-photo" src={pic1lg} alt="skatespots map"/>
+                    <img className="wanderlust-photo-md" src={pic1md} alt="skatespots map"/>
+                    <img className="wanderlust-photo-sm" src={pic1sm} alt="skatespots map"/>
                     <h3 className="proj1-role">My Role</h3>
                     <p className="role">I worked independently on this project, across the whole stack.</p>
                     <h3 className="proj1-implementation">Implementation</h3>
                     <p className="implementation">For starters I had to get a map up and running to allow for geolocation. I ended up using the 'react-google-maps' library which let me use a google map with react components. Once I had the map up, I started working on the backend routes.<br/><br/> I knew I wanted login functionality so I first worked on authentication using passportjs. Then I worked on creating, reading, updating, and deleting spots. From there, I was able to go to the front end and fetch api calls to send into my redux store and finish up with getting what I wanted rendered on the dom!</p>
-                    <img className="wanderlust-imp-photo" src={pic2lg}/>
-                    <img className="wanderlust-photo-md" src={pic2md}/>
-                    <img className="wanderlust-photo-sm" src={pic2sm}/>
+                    <img className="wanderlust-imp-photo" src={pic2lg} alt="skatespote place"/>
+                    <img className="wanderlust-photo-md" src={pic2md} alt="skatespote place"/>
+                    <img className="wanderlust-photo-sm" src={pic2sm} alt="skatespote place"/>
                     <h3 className="proj1-challenges">Challenges</h3>
                     <p className="challenges">I came across some struggles while working on this app. First off, I had no clue how to use the google api with react so I had to do research before finding the library I needed.<br/><br/> Secondly, I was still getting used to react-redux and 'react-google-maps' uses higher order components to update props which I had no clue about; I wasn't able to utilize the library to its full extent. Finally, I was naively passing url-encoded data from the server which was usable with Chrome but not the Edge browser, so I changed it to send JSON and fixed all my fetch calls on the frontend.</p>
                     <h3 className="proj1-conclusion">Conclusion</h3>
--- src/components/project3.js
+++ src/components/project3.js
@@ -39,16 +39,16 @@ class Project3 extends Component {
                         <a className="github-client" target="_blank" rel="noopener noreferrer" href="https://github.com/Jacob-Zim/Staccato-Client">Github Client</a>
                         <a className="github-server" target="_blank" rel="noopener noreferrer" href="https://github.com/Jacob-Zim/spaced-repetition-server">Github Server</a>
                     </div>
-                    <img className="wanderlust-photo" src={pic1lg} />
-                    <img className="wanderlust-photo-md" src={pic1md}/>
-                    <img className="wanderlust-photo-sm" src={pic1sm}/>
+                    <img className="wanderlust-photo" src={pic1lg} alt="staccato landing page"/>
+                    <img className="wanderlust-photo-md" src={pic1md} alt="staccato landing page"/>
+                    <img className="wanderlust-photo-sm" src={pic1sm} alt="staccato landing page"/>
                     <h3 className="proj1-role">My Role</h3>
                     <p className="role">I primarily worked on the backend of our app which included the algorithm, schema, authentication, and routes. Also, I created actions to access the backend api and helped a bit on the components.</p>
                     <h3 className="proj1-implementation">Implementation</h3>
                     <p className="implementation">My partner and I decided on splitting the work between the backend and frontend, helping each other out when required. I started with authentication as we needed the questions to have a many to one relationship with the user schema and I wanted to get that out of the way first. Then, I created the routes which my partner would use on the front end to render the questions we wanted to display. <br/><br/>Finally came the fun part, creating the algorithm. I used a linked list which places each question 2n places behind the first in the list, where n is the amount of times the user answered that question correct. If the user gets a question wrong n will reset to 1 and therefore will be placed behind the next question in the list. I had a fun time thinking about the logic and seeing it working by the time the app was complete!</p>
-                    <img className="wanderlust-imp-photo" src={pic2lg} />
-                    <img className="wanderlust-photo-md" src={pic2md}/>
-                    <img className="wanderlust-photo-sm" src={pic2sm}/>
+                    <img className="wanderlust-imp-photo" src={pic2lg} alt="staccato dashboard"/>
+                    <img className="wanderlust-photo-md" src={pic2md} alt="staccato dashboard"/>
+                    <img className="wanderlust-photo-sm" src={pic2sm} alt="staccato dashboard"/>
                     <h3 className="proj1-challenges">Challenges</h3>
                     <p className="challenges">There weren't too many challenges with creating Staccato. When creating the algorithm I first tried to use a stack but realized I'd need another stack to get the functionality that we were going for, so it would basically be a linked list which I used instead. Making sure I had the list specific for each user was a bit of a puzzle as I had not used an algorithm on a server before but I got it working by putting it behind a route!</p>
                     <h3 className="proj1-conclusion">Conclusion</h3>
